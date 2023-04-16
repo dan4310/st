b54b746b796984d5e0cc8fb97f3ff92bc96c0798
Daniel Mark Courtney <dan4310@gmail.com>
2023-04-16 16:01:07 -0400

    fix: xresources for alpha and fg/bg

--- config.def.h
+++ config.def.h
@@ -215,6 +215,8 @@ ResourcePref resources[] = {
 		{ "color13",      STRING,  &colorname[13] },
 		{ "color14",      STRING,  &colorname[14] },
 		{ "color15",      STRING,  &colorname[15] },
+		{ "color258",	  STRING,  &colorname[258] },
+		{ "color259",	  STRING,  &colorname[259] },
 		{ "background",   STRING,  &colorname[256] },
 		{ "foreground",   STRING,  &colorname[257] },
 		{ "cursorColor",  STRING,  &colorname[258] },
@@ -228,6 +230,7 @@ ResourcePref resources[] = {
 		{ "borderpx",     INTEGER, &borderpx },
 		{ "cwscale",      FLOAT,   &cwscale },
 		{ "chscale",      FLOAT,   &chscale },
+		{ "alpha",		  FLOAT,   &alpha },
 };
 
 /*
